ad5b0b4177c5ab25501ea1532a2ed7abdd49c297
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-05-26 21:05:51 +0700

    Successfully ask user for a directory in RootView

--- src/Views/RootView.tsx
+++ src/Views/RootView.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { connect } from "react-redux"
 import { Actions } from '../Application/Action'
 import CuntentComponent from '../Application/CuntentComponent';
+import * as electron from 'electron'
 
 
 export interface RRootViewProps {
@@ -10,11 +11,24 @@ export interface RRootViewProps {
 }
 
 export class RRootView extends CuntentComponent<RRootViewProps, {}> {
+    changeClicked = () => {
+        electron.remote.dialog.showOpenDialog({
+            properties: ['openDirectory']
+        }, filepaths => {
+            if (filepaths.length > 0) {
+                this.props.onPathChange(filepaths[0])
+            }
+        })
+    }
+
     render() {
-        return <div> { 
-            this.props.path != null 
-            ? this.props.path 
-            : "You are not currently in any directory." }
+        if (this.props.path != null) {
+            return <div>Success! { this.props.path }</div>
+        }
+
+        return <div>
+            "You are not currently in any directory."
+            <button onClick={this.changeClicked}>Choose folder</button>
         </div>
     }
 }
