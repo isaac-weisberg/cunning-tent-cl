06a24914de69c9d719f1ea30cd36b968f341bb59
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-05-26 15:15:00 +0700

    Add the entirety of that react boilerplate

---
+++ src/Application/CuntentApplication.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Component } from 'react'
+
+export class CuntentApplication extends Component {
+    render() {
+        return <h1>Helleu</h1>
+    }
+}
---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import { CuntentApplication } from './Application/CuntentApplication';
+
+render(
+    <CuntentApplication />,
+    document.getElementById('root')
+)
