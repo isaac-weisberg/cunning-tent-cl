ab0b84fd7dd19c2d4c88a3a619d99d864d4a8b2d
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-05-27 11:51:46 +0700

    Make DirectoryPickerView a lockable component

--- src/Application/LockableComponent.tsx
+++ src/Application/LockableComponent.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import CuntentComponent from "./CuntentComponent";
 
 export interface LockableComponentState {
@@ -7,7 +8,7 @@ export interface LockableComponentState {
 export class LockableComponent<P, S extends LockableComponentState> extends CuntentComponent<P, S> {
     render() {
         if (this.state.locked) {
-            return "Wait a second, I am busy ;)"
+            return <div> Wait a second, I am busy ;)</div>
         }
         return null
     }
--- src/Views/DirectoryPickerView.tsx
+++ src/Views/DirectoryPickerView.tsx
@@ -1,18 +1,36 @@
 import * as React from 'react'
-import CuntentComponent from '../Application/CuntentComponent';
 import LocaleKeys from '../Localize/Keys';
 import * as electron from 'electron'
+import { LockableComponentState, LockableComponent } from '../Application/LockableComponent';
 
 export interface DirectoryPickerViewProps {
     onPathPicked: (_: string) => void
 }
 
-export class DirectoryPickerView extends CuntentComponent<DirectoryPickerViewProps, {}> {
+
+export class DirectoryPickerView extends LockableComponent<DirectoryPickerViewProps, LockableComponentState> {
+    constructor(props) {
+        super(props)
+        this.state = {
+            locked: false
+        }
+    }
+
     changeClicked = () => {
+        this.setState(prev => {
+            return {
+                locked: true
+            }
+        })
         electron.remote.dialog.showOpenDialog({
             properties: ['openDirectory'],
             title: this.localize(LocaleKeys.TITLE_VIEW.CHOOSE_DIR)
         }, filepaths => {
+            this.setState(prev => {
+                return {
+                    locked: false
+                }
+            })
             if (filepaths.length > 0) {
                 this.props.onPathPicked(filepaths[0])
             }
@@ -20,6 +38,9 @@ export class DirectoryPickerView extends CuntentComponent<DirectoryPickerViewPro
     }
 
     render() {
+        if (this.state.locked) {
+            return super.render()
+        }
         return <div>
             {this.localize(LocaleKeys.TITLE_VIEW.NO_DIR_CHOSEN)}
             <button onClick={this.changeClicked}>{this.localize(LocaleKeys.TITLE_VIEW.CHOOSE_DIR)}</button>
