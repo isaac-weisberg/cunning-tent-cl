435f371c44ba926616dbde7fe76b706bd24487a2
Isaac Weisberg <isaac_weisberg@yahoo.com>
2018-05-28 18:11:19 +0700

    Add enrichment view

---
+++ src/Views/EnrichmentViews/EnrichmentView.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { Enrichment } from 'cuntent-assembler/dist';
+import CuntentComponent from '../../Application/CuntentComponent';
+
+export class EnrichmentView extends CuntentComponent<{ enrichment: Enrichment }, {}> {
+    
+}
