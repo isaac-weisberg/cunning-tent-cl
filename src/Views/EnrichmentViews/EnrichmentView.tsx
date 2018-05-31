9f8f35dc95ece79cee9c1ec792b3f9cc3e8bdf88
Isaac Weisberg <isaac_weisberg@yahoo.com>
2018-05-31 16:53:43 +0700

    Set Asset collation to be a redux component

--- src/Views/EnrichmentViews/EnrichmentView.tsx
+++ src/Views/EnrichmentViews/EnrichmentView.tsx
@@ -1,32 +1,60 @@
 import * as React from 'react'
-import { Enrichment } from 'cuntent-assembler/dist';
+import { Enrichment, GenericAssetCollation } from 'cuntent-assembler/dist';
 import LocaleKeys from '../../Localize/Keys';
 import { GenericAssetCollationView } from './CollationViews/GenericAssetCollationView';
-import { SingleAtomicCollationView } from './CollationViews/SingleAtomicCollationView';
+import CuntentComponent from '../../Application/CuntentComponent';
 import { EditingSessionObjectView } from '../EditingSessionView/EditingSessionView';
+import { AnyAction, Store, createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+export class RGenericCollationsView extends CuntentComponent<{ generics: GenericAssetCollation[], onAction: (_: GenericAssetCollationAction) => void }, {}> {
+    static mapProps(state) {
+        return {
+            generics: state
+        }
+    }
+
+    static mapDispatch(dispatch) {
+        return {
+            onAction: action => {
+                dispatch(action)
+            }
+        }
+    }
+    
+    render() {
+        return this.props.generics.length == 0 
+            ? this.localize(LocaleKeys.ASSET_COLLATION.NO_COLLATIONS)
+            : this.props.generics.map(collation => <GenericAssetCollationView key={collation.codepath} collation={collation} />)
+    }
+}
+
+const GenericCollationsView = connect(
+    RGenericCollationsView.mapProps, 
+    RGenericCollationsView.mapDispatch
+)(RGenericCollationsView)
+
+export interface GenericAssetCollationAction extends AnyAction {
+
+}
+
+function assetCollationReducer(prev: [GenericAssetCollation], action: GenericAssetCollationAction): [GenericAssetCollation] {
+    return prev
+}
 
 export class EnrichmentView extends EditingSessionObjectView<Enrichment> {
     constructor(props) {
         super(props)
+
+        this.store = createStore(assetCollationReducer, props.object.generics)
     }
 
-    drawGenerics = () => {
-        return this.props.object.generics.length == 0 
-        ? this.localize(LocaleKeys.ASSET_COLLATION.NO_COLLATIONS)
-        : this.props.object.generics.map(collation => <GenericAssetCollationView key={collation.codepath} collation={collation} />)
-    }
-
-    drawSingleAtoms = () => {
-        this.props.object.unsin.length == 0 
-        ? this.localize(LocaleKeys.ASSET_COLLATION.NO_COLLATIONS)
-        : this.props.object.unsin.map(collation => <SingleAtomicCollationView key={collation.codepath} collation={collation} />)
-    }
+    store: Store<[GenericAssetCollation]>
 
     render() {
-        return <div>
-            { this.drawGenerics() }
-            { this.drawSingleAtoms() }
-        </div>
+        return <Provider store={this.store} >
+            <GenericCollationsView />
+        </Provider>
     }
-
 }
+
